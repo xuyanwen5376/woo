661fc66cd2970342d01d775a33295e81b7ba50d2
xuyanwen <kylelou_ios@yahoo.com>
2025-06-06 15:51:29 +0800

    6.6 EasyLoading 配置

--- lib/global.dart
+++ lib/global.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:woo/common/services/wp_http.dart';
+import 'package:woo/common/utils/loading.dart';
 import 'package:woo/common/utils/storage.dart';
 import 'common/services/config.dart';
 
@@ -11,6 +12,17 @@ class Global {
 
     // 工具类
     await Storage().init();
+    
+
+    // 显示加载提示
+    // 这里可以根据需要显示加载提示
+    // 例如：Loading.show('正在加载...');
+    // 也可以不显示，直接调用 Loading() 即可
+    // 初始化加载提示
+    // 这里可以根据需要显示加载提示
+    // 例如：Loading.show('正在加载...');
+    // 也可以不显示，直接调用 Loading() 即可  
+    Loading();
     
   // 初始化服务
     Get.put<ConfigService>(ConfigService());
