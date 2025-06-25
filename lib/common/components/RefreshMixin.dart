5c259acd2b3f51397066b39a13a09f19f3076c33
xuyanwen <kylelou_ios@yahoo.com>
2025-06-25 16:54:19 +0800

    刷新封装

--- lib/common/components/RefreshMixin.dart
+++ lib/common/components/RefreshMixin.dart
@@ -3,23 +3,66 @@ import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 
 mixin RefreshMixin<T extends GetxController> on GetxController {
   // 刷新控制器
-  final RefreshController refreshController = RefreshController(initialRefresh: true);
+  final RefreshController refreshController = RefreshController(
+    initialRefresh: true,
+  );
 
   // 分页参数
   int page = 1;
   final int limit = 20;
+  bool hasMore = true;
 
-  // 刷新
-  Future<void> handleRefresh(Future<void> Function() refreshLogic) async {
+  /// 子类必须实现：分页数据加载
+  Future<List> loadPageData({required int page, required int limit});
+
+  /// 子类必须实现：刷新后如何处理数据
+  void onDataRefreshed(List data);
+
+  /// 子类必须实现：加载更多后如何处理数据
+  void onDataLoaded(List data);
+
+  /// 刷新
+  Future<void> onRefresh() async {
     page = 1;
-    await refreshLogic();
-    refreshController.refreshCompleted();
+    hasMore = true; 
+    try {
+      final data = await loadPageData(page: page, limit: limit); 
+      onDataRefreshed(data); 
+      if (data.length < limit) {
+        hasMore = false;
+        refreshController.loadNoData(); 
+      } else {
+        hasMore = true; 
+      }
+      refreshController.refreshCompleted();
+    } catch (e, s) { 
+      refreshController.refreshFailed();
+    }
   }
 
-  // 加载更多
-  Future<void> handleLoading(Future<void> Function() loadingLogic) async {
-    page++;
-    await loadingLogic();
-    refreshController.loadComplete();
+  /// 加载更多
+  Future<void> onLoading() async {
+    if (!hasMore) {
+      refreshController.loadNoData();
+      return;
+    }
+    try {
+      final data = await loadPageData(page: page + 1, limit: limit);
+      if (data.isEmpty) {
+        hasMore = false;
+        refreshController.loadNoData();
+      } else {
+        page++;
+        onDataLoaded(data);
+        if (data.length < limit) {
+          hasMore = false;
+          refreshController.loadNoData();
+        } else {
+          refreshController.loadComplete();
+        }
+      }
+    } catch (e) {
+      refreshController.loadFailed();
+    }
   }
 }
--- lib/pages/goods/home/controller.dart
+++ lib/pages/goods/home/controller.dart
@@ -1,9 +1,9 @@
+import 'package:flutter_woo_course_2025/common/components/RefreshMixin.dart';
 import 'package:get/get.dart';
-import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 
 import '../../../common/index.dart';
 
-class HomeController extends GetxController {
+class HomeController extends GetxController with RefreshMixin {
   HomeController();
 
   // Banner 当前位置
@@ -19,80 +19,23 @@ class HomeController extends GetxController {
   // 最新商品列表数据
   List<ProductModel> newProductProductList = [];
 
-  // 刷新控制器
-  final RefreshController refreshController = RefreshController(
-    initialRefresh: true,
-  );
-  // 页码
-  int _page = 1;
-  // 页尺寸
-  final int _limit = 20;
-
-  /// 拉取数据
-  /// isRefresh 是否是刷新
-  Future<bool> _loadNewsSell(bool isRefresh) async {
-    // 拉取数据
-    var result = await ProductApi.products(
-      ProductsReq(
-        // 刷新, 重置页数1
-        page: isRefresh ? 1 : _page,
-        // 每页条数
-        prePage: _limit,
-      ),
-    );
-
-    // 下拉刷新
-    if (isRefresh) {
-      _page = 1; // 重置页数1
-      newProductProductList.clear(); // 清空数据
-    }
-
-    // 有数据
-    if (result.isNotEmpty) {
-      // 页数+1
-      _page++;
-
-      // 添加数据
-      newProductProductList.addAll(result);
-    }
-
-    // 是否空
-    return result.isEmpty;
+  @override
+  Future<List<ProductModel>> loadPageData({
+    required int page,
+    required int limit,
+  }) async {
+    return await ProductApi.products(ProductsReq(page: page, prePage: limit));
   }
 
-  // 上拉载入新商品
-  void onLoading() async {
-    if (newProductProductList.isNotEmpty) {
-      try {
-        // 拉取数据是否为空
-        var isEmpty = await _loadNewsSell(false);
-
-        if (isEmpty) {
-          // 设置无数据
-          refreshController.loadNoData();
-        } else {
-          // 加载完成
-          refreshController.loadComplete();
-        }
-      } catch (e) {
-        // 加载失败
-        refreshController.loadFailed();
-      }
-    } else {
-      // 设置无数据
-      refreshController.loadNoData();
-    }
+  @override
+  void onDataRefreshed(List data) {
+    newProductProductList = List<ProductModel>.from(data);
     update(["home_news_sell"]);
   }
 
-  // 下拉刷新
-  void onRefresh() async {
-    try {
-      await _loadNewsSell(true);
-      refreshController.refreshCompleted();
-    } catch (error) {
-      refreshController.refreshFailed();
-    }
+  @override
+  void onDataLoaded(List data) {
+    newProductProductList.addAll(List<ProductModel>.from(data));
     update(["home_news_sell"]);
   }
 
@@ -136,11 +79,6 @@ class HomeController extends GetxController {
 
   void onTap() {}
 
-  // @override
-  // void onInit() {
-  //   super.onInit();
-  // }
-
   @override
   void onReady() {
     super.onReady();
@@ -152,5 +90,30 @@ class HomeController extends GetxController {
     super.onClose();
     // 刷新控制器释放
     refreshController.dispose();
+  }
+
+  Future<void> onLoading() async {
+    if (!hasMore) {
+      refreshController.loadNoData();
+      return;
+    }
+    try {
+      final data = await loadPageData(page: page + 1, limit: limit);
+      if (data.isEmpty) {
+        hasMore = false;
+        refreshController.loadNoData();
+      } else {
+        page++;
+        onDataLoaded(data);
+        if (data.length < limit) {
+          hasMore = false;
+          refreshController.loadNoData();
+        } else {
+          refreshController.loadComplete();
+        }
+      }
+    } catch (e) {
+      refreshController.loadFailed();
+    }
   }
 }
