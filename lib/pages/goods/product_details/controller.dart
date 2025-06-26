01248be5571913c38955ce7663c3951ee2199b36
xuyanwen <kylelou_ios@yahoo.com>
2025-06-26 14:31:49 +0800

    12.2 轮播图

--- lib/pages/goods/product_details/controller.dart
+++ lib/pages/goods/product_details/controller.dart
@@ -1,11 +1,40 @@
 import 'package:get/get.dart';
 
+import '../../../common/index.dart';
+
 class ProductDetailsController extends GetxController {
   ProductDetailsController();
 
-  _initData() {
+  int? productId = Get.arguments['id'] ?? 0;
+
+  ProductModel? product;
+
+  // Banner 数据
+  List<KeyValueModel> bannerItems = [];
+
+  int bannerCurrentIndex = 0;
+
+  _initData() async {
+    product = await ProductApi.productDetail(productId.toString());
+
+    // Banner 数据
+    if (product?.images != null) {
+      bannerItems =
+          product!.images!
+              .map<KeyValueModel>(
+                (e) => KeyValueModel(key: "${e.id}", value: e.src ?? ""),
+              )
+              .toList();
+    }
+
     update(["product_details"]);
   }
+
+  void onChangeBanner(int index, _reason) {
+    bannerCurrentIndex = index;
+    update(["banner"]);
+  }
+ 
 
   void onTap() {}
 
--- lib/pages/goods/product_details/view.dart
+++ lib/pages/goods/product_details/view.dart
@@ -1,16 +1,73 @@
+import 'package:ducafe_ui_core/ducafe_ui_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../common/index.dart';
 import 'index.dart';
 
 class ProductDetailsPage extends GetView<ProductDetailsController> {
   const ProductDetailsPage({super.key});
 
+  // 滚动图
+  Widget _buildBanner(BuildContext context) {
+    return GetBuilder<ProductDetailsController>(
+      id: "product_banner",
+      // tag: tag,
+      builder: (_) {
+        return CarouselWidget(
+          // 打开大图预览
+          // onTap: controller.onGalleryTap,
+          // 图片列表
+          items: controller.bannerItems,
+          // 当前索引
+          currentIndex: controller.bannerCurrentIndex,
+          // 切换回调
+          onPageChanged: controller.onChangeBanner,
+          // 高度
+          height: 190.w,
+          // 指示器圆点
+          indicatorCircle: false,
+          // 指示器位置
+          indicatorAlignment: MainAxisAlignment.start,
+          // 指示器颜色
+          indicatorColor: context.colors.scheme.secondary,
+        );
+      },
+    ).backgroundColor(context.colors.scheme.surface);
+  }
+
+  // 商品标题
+  Widget _buildTitle() {
+    return Container(child: Text("商品标题"));
+  }
+
+  // Tab 栏位
+  Widget _buildTabBar() {
+    return Container(child: Text("Tab 栏位"));
+  }
+
+  // TabView 视图
+  Widget _buildTabView() {
+    return Container(child: Text("TabView 视图"));
+  }
+
   // 主视图
-  Widget _buildView() {
-    return const Center(
-      child: Text("ProductDetailsPage"),
-    );
+  Widget _buildView(BuildContext context) {
+    return controller.product == null
+        ? const PlaceholdWidget() // 占位图
+        : <Widget>[
+          // 滚动图
+          _buildBanner(context),
+
+          // 商品标题
+          _buildTitle(),
+
+          // Tab 栏位
+          _buildTabBar(),
+
+          // TabView 视图
+          _buildTabView(),
+        ].toColumn();
   }
 
   @override
@@ -20,10 +77,10 @@ class ProductDetailsPage extends GetView<ProductDetailsController> {
       id: "product_details",
       builder: (_) {
         return Scaffold(
-          appBar: AppBar(title: const Text("product_details")),
-          body: SafeArea(
-            child: _buildView(),
+          appBar: mainAppBarWidget(
+            titleString: controller.product?.name ?? LocaleKeys.gDetailTitle.tr,
           ),
+          body: SafeArea(child: _buildView(context)),
         );
       },
     );
