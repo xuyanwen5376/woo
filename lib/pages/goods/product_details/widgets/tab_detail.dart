a47572a489bf1200136169ea97cc62dbc6c48117
xuyanwen <kylelou_ios@yahoo.com>
2025-06-26 17:34:24 +0800

    12.9 Tab商品详情

--- lib/pages/goods/product_details/widgets/tab_detail.dart
+++ lib/pages/goods/product_details/widgets/tab_detail.dart
@@ -1,16 +1,64 @@
- import 'package:flutter/material.dart';
- import 'package:get/get.dart';
+import 'package:ducafe_ui_core/ducafe_ui_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+import '../../../../common/index.dart';
 import '../index.dart';
 
 /// 商品详情
 class TabDetailView extends GetView<ProductDetailsController> {
   const TabDetailView({super.key});
- 
-  
+
+  _buildTitle(String title) {
+    return TextWidget.body(
+      title,
+      weight: FontWeight.w500,
+    ).paddingBottom(AppSpace.listRow);
+  }
+
+  // 内容
+  _buildContent(String? title) {
+    return TextWidget.label(
+      title ?? "-",
+      softWrap: true,
+      maxLines: 10,
+    ).paddingBottom(AppSpace.listRow * 2);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Text("2222111");
+    return <Widget>[
+          // 说明
+          _buildTitle("Description"),
+          _buildContent(controller.product?.description?.clearHtml),
+
+          // sku
+          _buildTitle("SKU"),
+          _buildContent(controller.product?.sku ?? "-"),
+
+          // price
+          _buildTitle("Price"),
+          _buildContent(controller.product?.price ?? "-"),
+
+          // 市场价
+          _buildTitle("Regular price"),
+          _buildContent(controller.product?.regularPrice ?? "-"),
+
+          // 重量
+          _buildTitle("Weight"),
+          _buildContent(controller.product?.weight ?? "-"),
+
+          // 尺寸
+          _buildTitle("dimensions"),
+          _buildContent(
+            "${controller.product?.dimensions?.length} x ${controller.product?.dimensions?.width} x ${controller.product?.dimensions?.height}",
+          ),
+
+          // end
+        ]
+        .toColumn(crossAxisAlignment: CrossAxisAlignment.start)
+        .scrollable()
+        .paddingVertical(AppSpace.page);
   }
+ 
 }
