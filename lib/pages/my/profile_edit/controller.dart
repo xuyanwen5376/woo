15351e98dd3753a97feb8500bb5d182991b59d0a
xuyanwen <kylelou_ios@yahoo.com>
2025-07-01 11:55:52 +0800

    20.1 修改我的信息页面

--- lib/pages/my/profile_edit/controller.dart
+++ lib/pages/my/profile_edit/controller.dart
@@ -1,9 +1,31 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../../common/index.dart';
 
 class ProfileEditController extends GetxController {
   ProfileEditController();
 
+  // 表单 form
+  GlobalKey formKey = GlobalKey<FormState>();
+
+  // 输入框控制器
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController oldPasswordController = TextEditingController();
+  TextEditingController newPasswordController = TextEditingController();
+  TextEditingController confirmNewPasswordController = TextEditingController();
+
+  // 初始数据_
   _initData() {
+    // 用户 profile
+    UserProfileModel profile = UserService.to.profile;
+
+    // 初始值
+    firstNameController.text = profile.firstName ?? "";
+    lastNameController.text = profile.lastName ?? "";
+    emailController.text = profile.email ?? "";
     update(["profile_edit"]);
   }
 
@@ -20,8 +42,14 @@ class ProfileEditController extends GetxController {
     _initData();
   }
 
-  // @override
-  // void onClose() {
-  //   super.onClose();
-  // }
+  @override
+  void onClose() {
+    super.onClose();
+    firstNameController.dispose();
+    lastNameController.dispose();
+    emailController.dispose();
+    oldPasswordController.dispose();
+    newPasswordController.dispose();
+    confirmNewPasswordController.dispose();
+  }
 }
--- lib/pages/my/profile_edit/view.dart
+++ lib/pages/my/profile_edit/view.dart
@@ -1,16 +1,73 @@
+import 'package:ducafe_ui_core/ducafe_ui_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../common/index.dart';
 import 'index.dart';
 
 class ProfileEditPage extends GetView<ProfileEditController> {
   const ProfileEditPage({super.key});
 
   // 主视图
-  Widget _buildView() {
-    return const Center(
-      child: Text("ProfileEditPage"),
+  Widget _buildView(BuildContext context) {
+    return SingleChildScrollView(
+      child: <Widget>[
+        // 头像
+        _buildAvatar(context),
+
+        // 表单
+        Form(
+          key: controller.formKey, //设置globalKey，用于后面获取FormState
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: <Widget>[
+            // profile 表单
+            _buildProfileForm(context),
+            // password 表单
+            _buildPasswordForm(context),
+          ].toColumn(),
+        ).paddingBottom(AppSpace.card),
+
+        // 保存按钮
+        ButtonWidget.primary(
+          LocaleKeys.commonBottomSave.tr,
+        ).width(double.infinity),
+      ].toColumn().paddingAll(AppSpace.card),
     );
+  }
+
+
+  // 头像
+  Widget _buildAvatar(BuildContext context) {
+    return ListTileWidget(
+          title: TextWidget.label(LocaleKeys.profileEditMyPhoto.tr),
+          trailing: [
+            ImageWidget.img(
+              // UserService.to.profile.avatarUrl,
+              "https://ducafecat-pub.oss-cn-qingdao.aliyuncs.com/avatar/00258VC3ly1gty0r05zh2j60ut0u0tce02.jpg",
+              width: 50.w,
+              height: 50.w,
+              fit: BoxFit.cover,
+              radius: 25.w,
+            ),
+          ],
+          padding: EdgeInsets.all(AppSpace.card),
+        )
+        .card(
+          color: context.colors.scheme.surface,
+          margin: EdgeInsets.zero,
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        )
+        .paddingBottom(AppSpace.card);
+  }
+
+  //  profile 表单
+  Widget _buildProfileForm(BuildContext context) {
+    return const Text("profile 表单");
+  }
+
+  //  password 表单
+  Widget _buildPasswordForm(BuildContext context) {
+    return const Text("password 表单");
   }
 
   @override
@@ -21,9 +78,7 @@ class ProfileEditPage extends GetView<ProfileEditController> {
       builder: (_) {
         return Scaffold(
           appBar: AppBar(title: const Text("profile_edit")),
-          body: SafeArea(
-            child: _buildView(),
-          ),
+          body: SafeArea(child: _buildView(context)),
         );
       },
     );
